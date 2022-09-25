5b5ed6b107f32cea2e4b6f29ace2388b4313af22
helio <helio61843@gmail.com>
2022-09-25 23:40:17 +0800

    qlearning v0.2

--- routePath.js
+++ routePath.js
@@ -12,17 +12,31 @@ const map = [
 class QLearning {
 
     route = [];
+    qTable = [];
     currState = null;
     sinkNode = null;
 
+    // epsilon 參數
     epsilon = 1;
     minEpsilon = 0.05;
+    subNum = 0.001;
+
+    gamma = 0.95;
+    learning_rate = 0.8;
+    moveReward = -1;
 
     constructor(nodes) {
-        this.nodes = nodes;
+        this.nodes = [...nodes];
         // UAV 初始位置為 sink
-        this.currState = this.getSinkNode();
-        this.sinkNode = this.currState;
+        this.qTable = this.buildQtable();
+        this.sinkNode =  this.getSinkNode();
+        this.init();
+    }
+
+    init() {
+        // this.route = [];
+        this.currState = this.sinkNode;
+        this.route.push(this.currState);
     }
 
     getSinkNode() {
@@ -47,20 +61,73 @@ class QLearning {
 
     getNextState() {
         let nextState;
+        let action;
 
         // 小於 epsilon，則隨機取得下一個 state
-        if (Math.random() < epsilon) {
-            const nextIndex = getRandom(this.nodes.length);
-
+        if (Math.random() < this.epsilon) {
+            action = getRandom(this.nodes.length);
+            nextState = this.getNode(action);
         } else {
-
+            const maxReward = Math.max(...this.qTable[this.currState]);
+            const action = this.qTable[this.currState].findIndex(reward => reward === maxReward);
+            nextState = action;
+            const nextReward = Math.max(...this.qTable[nextState]);
+            this.qTable[this.currState][action] = this.qTable[this.currState][action] + learning_rate * (
+                moveReward + gamma * nextReward - this.qTable[this.currState][action]
+            )
         }
 
         return nextState;
     }
 
-    run(nodes) {
+    updateEpsilon() {
+        if(this.epsilon > this.minEpsilon) {
+            this.epsilon -= this.subNum;
+        }
+    }
 
+    getRoute() {
+        const route = []
+        const loopNum = this.nodes.length;
+        for(let i=0; i<loopNum; i++) {
+            this.currState = this.getNextState();
+            this.updateEpsilon();
+            route.push(this.currState);
+        }
+        return route;
+    }
+
+    getDistance() {
+        console.log('route', this.route)
+        let distance = 0;
+        for(let i =0; i < this.route.length-2; i++) {
+            distance = distance + Math.sqrt(
+                Math.pow(this.route[i].xAxis - this.route[i+1].xAxis, 2) +
+                Math.pow(this.route[i].yAxis - this.route[i+1].yAxis, 2) 
+            )
+        }
+        return distance;
+    }
+
+    updateFinishReward(distance) {
+        for(let i =0; i < this.route.length-2; i++) {
+            const state = this.route[i].index;
+            const nextState = this.route[i+1].index;
+            this.qTable[state][nextState] = this.qTable[state][nextState] + distance / 10;
+        }
+    }
+
+    run() {
+        for(let i=0; i<100; i++) {
+            console.log('loop', i);
+            this.route.push(...this.getRoute());
+            // back sink
+            this.route.push(this.sinkNode);
+            const distance = this.getDistance();
+            this.updateFinishReward(distance);
+            console.log(this.route);
+            this.init();
+        }
     }
 }
 
@@ -77,8 +144,8 @@ map.forEach((rows, yAxis) => {
 
 nodes = nodes.map((item, index) => ({ ...item, index }));
 
-new QLearning(nodes);
-
+const qlearning = new QLearning(nodes);
+qlearning.run();
 
 function getRandom(x){
     return Math.floor(Math.random() * x);
